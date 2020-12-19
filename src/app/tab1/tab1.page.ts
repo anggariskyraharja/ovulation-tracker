b6648c5f7827c2b4e43c714ebbac7d686bede6cf
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2020-12-19 17:12:23 +0700

    final

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { ɵDomAdapter } from '@angular/common';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
@@ -11,17 +12,25 @@ export class Tab1Page {
   cycle: number;
   result1: string;
   nextperiod: Date;
+  ovulation: Date;
+  intercourse1: Date;
+  intercourse2: Date;
+  pregnancytest: Date;
+  duedate: Date;
+  ovulationwindow: Date;
 
   constructor(public alertController: AlertController) {
   }
 
-  check1(event){
-    this.date = event.target.value;
-  }
-
   clear(){
     this.cycle = null;
-    this.result1 = null;
+    this.date = null;
+    this.nextperiod = null;
+    this.ovulation = null;
+    this.intercourse1 = null;
+    this.intercourse2 = null;
+    this.pregnancytest = null;
+    this.duedate = null;
   }
 
   async presentAlert1() {
@@ -33,10 +42,33 @@ export class Tab1Page {
     await alert.present();
   }
 
+  async presentAlert2() {
+    const alert = await this.alertController.create({
+      header: 'Wrong Value',
+      message: 'Cycle ranges from 22 to 44 days! Use 28 days if unsure.',
+      buttons: ['Okay']
+    });
+    await alert.present();
+  }
+
   calculate(){
     if (this.cycle == null || this.date == null){this.presentAlert1()}
+    else if (this.cycle < 22 || this.cycle > 44){this.presentAlert2()}
     else {
-      this.nextperiod = new Date (this.date.getFullYear(), this.date.getMonth(), this.date.getDate()+9);
+      this.nextperiod = new Date(this.date);
+      this.nextperiod.setDate(this.nextperiod.getDate()+this.cycle);
+      this.ovulation = new Date(this.nextperiod);
+      this.ovulation.setDate(this.ovulation.getDate()-14);
+      this.intercourse1 = new Date(this.ovulation);
+      this.intercourse2 = new Date(this.ovulation);
+      this.ovulationwindow = new Date(this.ovulation);
+      this.ovulationwindow.setDate(this.ovulationwindow.getDate()-2);
+      this.intercourse1.setDate(this.intercourse1.getDate()-5);
+      this.intercourse2.setDate(this.intercourse2.getDate()+2);
+      this.pregnancytest = new Date(this.nextperiod);
+      this.pregnancytest.setDate(this.pregnancytest.getDate()-5);
+      this.duedate = new Date(this.date);
+      this.duedate.setDate(this.duedate.getDate()+280-28+this.cycle);
     }
   }   
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,4 @@
+import { ɵDomAdapter } from '@angular/common';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
@@ -7,27 +8,41 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  gender: string = 'male';
-  age: string = 'adult';
-  weight: number;
-  sodium: number;
-  correction: number;
-  deficit: string;
+  date: Date;
+  long: number;
+  cycle: number;
+  nextperiod1: Date;
+  lastperiod1: Date;
+  nextperiod2: Date;
+  lastperiod2: Date;
+  nextperiod3: Date;
+  lastperiod3: Date;
+  nextovulation1: Date;
+  nextovulation2: Date;
+  nextovulation3: Date;
+  lastovulation1: Date;
+  lastovulation2: Date;
+  lastovulation3: Date;
 
   constructor(public alertController: AlertController) {
   }
 
-  check1(event){
-    this.gender = event.target.value;
-  }
-  check2(event){
-    this.age = event.target.value;
-  }
   clear(){
-    this.weight = null;
-    this.sodium = null;
-    this.correction = null;
-    this.deficit = null;
+    this.cycle = null;
+    this.long = null;
+    this.date = null;
+    this.nextperiod1 = null;
+    this.nextperiod2 = null;
+    this.nextperiod3 = null;
+    this.lastperiod1 = null;
+    this.lastperiod2 = null;
+    this.lastperiod3 = null;
+    this.nextovulation1 = null;
+    this.nextovulation2 = null;
+    this.nextovulation3 = null;
+    this.lastovulation1 = null;
+    this.lastovulation2 = null;
+    this.lastovulation3 = null;
   }
 
   async presentAlert1() {
@@ -38,43 +53,56 @@ export class Tab2Page {
     });
     await alert.present();
   }
+
   async presentAlert2() {
     const alert = await this.alertController.create({
       header: 'Wrong Value',
-      message: 'Current sodium level must be less than desired sodium level!',
+      message: 'Cycle ranges from 22 to 44 days! Use 28 days if unsure.',
       buttons: ['Okay']
     });
     await alert.present();
   }
-  
-  roundoff(value){
-    var value4 = "" + Math.round(value);
-    var bonus2 = value4.length + 1;
-    var bonus = 0;
-    if (value < 100000){bonus=bonus+1};
-    if (value < 10){bonus=bonus+1};
-    if (value < 1){bonus=bonus+1};
-    if (value < 0.1){bonus=bonus+1};
-    if (value < 0.01){bonus=bonus+1};
-    if (value < 0.001){bonus=bonus+1};
-    if (value < 0.0001){bonus=bonus+1};
-    bonus2 = bonus2+bonus;
-    var whole = Math.round(value * Math.pow(10, bonus));
-    var whole2 = "" + whole * Math.pow(10, -1*bonus);
-    var whole3 = whole2.substr(0,bonus2);
-    return whole3;
+  async presentAlert3() {
+    const alert = await this.alertController.create({
+      header: 'Wrong Value',
+      message: 'Duration ranges from 1 to 10 days!',
+      buttons: ['Okay']
+    });
+    await alert.present();
   }
+
   calculate(){
-    if (this.weight == null || this.sodium == null || this.correction == null){this.presentAlert1()} 
-    else if (this.sodium > this.correction) {this.presentAlert2()}
+    if (this.cycle == null || this.date == null){this.presentAlert1()}
+    else if (this.cycle < 22 || this.cycle > 44){this.presentAlert2()}
+    else if (this.long < 1 || this.long > 10){this.presentAlert3()}
     else {
-      var TBW: number;
-      if ((this.age == 'child') || (this.age == 'adult' && this.gender == 'male')){TBW = this.weight * 0.6}
-      else if ((this.age == 'adult' && this.gender == 'female') || (this.age == 'elderly' && this.gender == 'male')){TBW = this.weight * 0.5}
-      else if (this.age == 'elderly' && this.gender == 'female'){TBW = this.weight * 0.45}
-      var sodium: number;
-      sodium = (this.correction -  this.sodium)*TBW;
-      this.deficit = 'The sodium deficit is: '+this.roundoff(sodium)+' mEq.'
+      this.nextperiod1 = new Date(this.date);
+      this.nextperiod1.setDate(this.nextperiod1.getDate()+this.cycle);
+      this.lastperiod1 = new Date (this.nextperiod1);
+      this.lastperiod1.setDate(this.lastperiod1.getDate()+this.long-1);
+      this.nextovulation1 = new Date(this.nextperiod1);
+      this.nextovulation1.setDate(this.nextovulation1.getDate()-16);
+      this.lastovulation1 = new Date(this.nextperiod1);
+      this.lastovulation1.setDate(this.lastovulation1.getDate()-12);
+      this.nextperiod2 = new Date(this.nextperiod1);
+      this.nextperiod2.setDate(this.nextperiod2.getDate()+this.cycle);
+      this.lastperiod2 = new Date (this.nextperiod2);
+      this.lastperiod2.setDate(this.lastperiod2.getDate()+this.long-1);
+      this.nextovulation2 = new Date(this.nextperiod2);
+      this.nextovulation2.setDate(this.nextovulation2.getDate()-16);
+      this.lastovulation2 = new Date(this.nextperiod2);
+      this.lastovulation2.setDate(this.lastovulation2.getDate()-12);
+      this.nextperiod3 = new Date(this.nextperiod2);
+      this.nextperiod3.setDate(this.nextperiod3.getDate()+this.cycle);
+      this.lastperiod3 = new Date (this.nextperiod3);
+      this.lastperiod3.setDate(this.lastperiod3.getDate()+this.long-1);
+      this.nextovulation3 = new Date(this.nextperiod3);
+      this.nextovulation3.setDate(this.nextovulation3.getDate()-16);
+      this.lastovulation3 = new Date(this.nextperiod3);
+      this.lastovulation3.setDate(this.lastovulation3.getDate()-12);
+      this.nextperiod3 = new Date(this.date);
+      this.lastperiod3 = new Date(this.date);
+      this.lastperiod3.setDate(this.lastperiod3.getDate()+this.long-1);
     }
   }   
 }
