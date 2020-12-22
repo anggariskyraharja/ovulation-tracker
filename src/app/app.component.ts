6d27e059a6b85125e96127323737fb915629061f
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2020-12-22 07:51:27 +0700

    android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
   ) {
     this.initializeApp();
     // Initialize AdMob for your Application
-    AdMob.initialize("ca-app-pub-9580735978410872~9756107921");
+    AdMob.initialize("ca-app-pub-9580735978410872~6313463030");
   }
 
   initializeApp() {
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -16,7 +16,7 @@ FirebaseAnalytics.enable();
 })
 export class TabsPage implements OnInit, OnDestroy {
   options: AdOptions = {
-    adId: "ca-app-pub-9580735978410872/5166960373",
+    adId: "ca-app-pub-9580735978410872/2872911471",
     adSize: AdSize.BANNER,
     position: AdPosition.BOTTOM_CENTER,
   };
